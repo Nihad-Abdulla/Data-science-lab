5d20d40863b78d8b48fac7bb9514dbc903a9fdbd
Nihad-Abdulla <100753393+Nihad-Abdulla@users.noreply.github.com>
2022-08-16 11:39:35 +0530

    Add files via upload

---
+++ 36.Nihad Abdulla/12 matrix operation.py
@@ -0,0 +1,38 @@
+import numpy
+matrix1=numpy.matrix([[1,2],[3,4]])
+matrix2=numpy.matrix([[4,3],[2,1]])
+matrix3=numpy.add(matrix1,matrix2)
+print(matrix3)
+
+import numpy
+matrix1=numpy.matrix([[2,2],[2,2]])
+matrix2=numpy.matrix([[1,1],[1,1]])
+matrix3=numpy.subtract(matrix1,matrix2)
+print(matrix3)
+import numpy
+matrix1=numpy.matrix([[2,2],[2,2]])
+matrix2=numpy.matrix([[1,1],[1,1]])
+matrix3=numpy.matmul(matrix1,matrix2)
+print(matrix3)
+import numpy
+matrix1=numpy.matrix([[2,2],[2,2]])
+matrix2=2*matrix1
+print(matrix2)
+
+import numpy
+matrix1=numpy.matrix([[1,2],[3,4]])
+print(matrix1)
+matrix2=numpy.transpose(matrix1)
+print(matrix2)
+
+
+# Scalar Multiplication
+import numpy
+matrix1=numpy.matrix([[2,2],[2,2]])
+matrix2=2*matrix1
+print(matrix2)
+
+# Matrix Transpose
+import numpy
+matrix1=numpy.matrix([[1,2],[3,4]])
+print(matrix1)
